e3964cd28d5c93494aebe63c5950088bf25ccd6d
Giovanna Coqueiro <gcoqueir@student.42sp.org.br>
2024-10-24 21:23:33 -0300

    almost there

--- src/mode.cpp
+++ src/mode.cpp
@@ -1,160 +1,160 @@
 #include "../includes/Server.hpp"
 
-static bool isValidChannelName(const std::string& channelName) {
-    if (channelName.empty() || channelName[0] != '#')
-        return false;
-
-    return true;
-}
-
 void Server::mode(std::vector<std::string> string, int fd) {
 	std::string response;
-
+	std::string mode;
+	
 	Client* client = getClientByFD(fd);
-
-	if (string.size() == 0 || string[0] == "" || string[0] == "MODE" || string.size() > 4) {
-		response = "You must provide a client to be demoted\r\nUsage: MODE #<channel name> +/-<mode> <option>\r\n";
+	if (string.size() == 0 || string[0] == "") {
+		std::cout << RED << "Error manipulating modes..." << WHITE << std::endl;
+		response = IRC + ERR_NEEDMOREPARAMSNBR + " MODE " + ERR_NEEDMOREPARAMS + END;
 		send(fd, response.c_str(), response.size(), 0);
 		return;
 	}
 
-	bool validChannelName = isValidChannelName(string[0]);
-	if (validChannelName == false) {
-		response = "Invalid channel name\r\nUsage: MODE #<channel name> +/-<mode> <option>\r\n";
-		send(fd, response.c_str(), response.size(), 0);
-		return;
-	}
-
-	std::string channelName = string[0].substr(1);
+	std::string channelName = string[0];
 	Channel* channel = getChannel(channelName);
-
-	//verifica se o canal existe no server
-	if (channel == NULL) {
-		response = "Channel does not exist\r\n";
+	if(channel == NULL) {
+		std::cout << RED << "Error manipulating modes..." << WHITE << std::endl;
+		response = IRC + ERR_NOSUCHCHANNELNBR + client->getNickname() + " " + channelName + ERR_NOSUCHCHANNEL + END;
 		send(fd, response.c_str(), response.size(), 0);
 		return;
 	}
 
-	//verifica se o client que chamou o comando está no canal
-	if (!channel->isOnChannel(client->getNickname())) {
-		response = "You must be on the channel to use mode command\r\n";
-		send(fd, response.c_str(), response.size(), 0);
-		return;
-	}
-
-	//verifica se o usuário é um operador
-	if (!channel->isOperator(client->getNickname())) {
-		response = "You don't have operator privileges on this channel\r\n";
-		send(fd, response.c_str(), response.size(), 0);
-		return;
-	}
-
-	//verifica se o usuário enviou um modo para alterar, caso não tenha enviado, retorna a lista de modos ativos no canal
-	if (string.size() == 1 || string[1] == "") {
+	if(string.size() == 1 || string[1] == "") {
+		std::cout << YELLOW << "Showing modes..." << WHITE << std::endl;
+		std::string imode = "-i";
+		if (channel->getMode("i") == true)
+			imode = "+i";
+		std::string tmode = "-t";
+		if (channel->getMode("t") == true)
+			tmode = "+t";
+		std::string kmode = "-k";
+		if (channel->getMode("k") == true)
+			kmode = "+k";
+		std::string lmode = "-l";
+		if (channel->getMode("l") == true)
+			lmode = "+l";
 		std::string modes = channel->getAllModes();
-		response = "#" + channel->getName() + ": Modes on this channel: "+ modes + "\r\n";
+		response = IRC + RPL_CHANNELMODEISNBR + client->getNickname() + " " + channelName + " " + imode + tmode + kmode + lmode + END;
+		send(fd, response.c_str(), response.size(), 0);
+		return;
+	}
+	
+	if (!channel->isOperator(client->getNickname())) {
+		std::cout << RED << "Error manipulating modes..." << WHITE << std::endl;
+		response = IRC + ERR_CHANOPRIVSNEEDEDNBR + client->getNickname() + " " + channelName + ERR_CHANOPRIVSNEEDED + END;
 		send(fd, response.c_str(), response.size(), 0);
 		return;
 	}
 
 	int i = 0;
 	std::string modes[] = {"-i", "+i", "-t", "+t", "-k", "+k", "-o", "+o", "-l", "+l"};
-	std::string mode;
-	mode = string[1];
+	mode = string[1].substr(0, string[1].find('\r'));
 	std::string modeArg = "";
-	if (string.size() > 2)
+	if(string.size() > 2)
 		modeArg = string[2];
 	Client* clientArg = getClientByNick(modeArg);
 	std::stringstream ss(modeArg);
+	response = ":" + client->getNickname() + "!" + client->getClientname() + "@ft.irc MODE " + channelName + " ";
 
-	for (; i < 10; i++)
-		if (mode == modes[i])
+	for(; i < 10; i++)
+		if(mode == modes[i])
 			break;
 
 	switch (i) {
 		case 0:
 			channel->setMode("i", false);
+			response += "-i";
 			break;
 
 		case 1:
 			channel->setMode("i", true);
+			response += "+i";
 			break;
 
 		case 2:
 			channel->setMode("t", false);
+			response += "-t";
 			break;
 
 		case 3:
 			channel->setMode("t", true);
+			response += "+t";
 			break;
 
 		case 4:
 			channel->setMode("k", false);
 			channel->setPassword("");
+			response += "-k";
 			break;
 
-		case 5:			
+		case 5:
 			if (modeArg == "") {
-				response = "You must provide a password to be set\r\nUsage: MODE #<channel name> +k <password>\r\n";
+				std::cout << RED << "Error manipulating modes..." << WHITE << std::endl;
+				response = IRC + ERR_NEEDMOREPARAMSNBR + client->getNickname() + channelName + " " + channelName + " k * :You must specify a parameter for the key mode. Syntax: <key>." + END;
 				send(fd, response.c_str(), response.size(), 0);
 				return;
 			}
 			channel->setMode("k", true);
 			channel->setPassword(modeArg);
+			response += "+k";
 			break;
 
 		case 6:
 			if (modeArg == "") {
-				response = "You must provide a client to be demoted\r\nUsage: MODE #<channel name> -o <nickname>\r\n";
+				std::cout << RED << "Error manipulating modes..." << WHITE << std::endl;
+				response = IRC + ERR_NEEDMOREPARAMSNBR + client->getNickname() + channelName + " " + channelName +  " o * :You must specify a parameter for the nick mode. Syntax: <nick>." + END;
 				send(fd, response.c_str(), response.size(), 0);
 				return;
 			}
 
 			if (clientArg == NULL) {
-				response = "This client is not on this channel\r\n";
+				std::cout << RED << "Error manipulating modes..." << WHITE << std::endl;
+				response = IRC + ERR_NOSUCHNICKNBR + modeArg + ERR_NOSUCHNICK + END;
 				send(fd, response.c_str(), response.size(), 0);
 				return;
 			}
 
-			if (!channel->isOperator(clientArg->getNickname())) {
-				response = "This client is not an operator on this channel\r\n";
-				send(fd, response.c_str(), response.size(), 0);
+			if (!channel->isOnChannel(clientArg->getNickname()) || !channel->isOperator(clientArg->getNickname()))
 				return;
-			}
 
 			channel->demoteFromOperator(clientArg->getNickname());
+			response += "-o";
 			break;
 
 		case 7:
 			if (modeArg == "") {
-				response = "You must provide a client to be promoted\r\nUsage: MODE #<channel name> +o <nickname>\r\n";
+				std::cout << RED << "Error manipulating modes..." << WHITE << std::endl;
+				response = IRC + ERR_NEEDMOREPARAMSNBR + client->getNickname() + channelName + " " + channelName +  " o * :You must specify a parameter for the nick mode. Syntax: <nick>." + END;
 				send(fd, response.c_str(), response.size(), 0);
 				return;
 			}
 
 			if (clientArg == NULL) {
-				response = "This client is not on this channel\r\n";
+				std::cout << RED << "Error manipulating modes..." << WHITE << std::endl;
+				response = IRC + ERR_NOSUCHNICKNBR + modeArg + ERR_NOSUCHNICK + END;
 				send(fd, response.c_str(), response.size(), 0);
 				return;
 			}
 
-			if (channel->isOperator(clientArg->getNickname())) {
-				response = "This client is already an operator on this channel\r\n";
-				send(fd, response.c_str(), response.size(), 0);
+			if (!channel->isOnChannel(clientArg->getNickname()) || channel->isOperator(clientArg->getNickname())) 
 				return;
-			}
 
 			channel->promoteToOperator(clientArg->getNickname());
+			response += "+o";
 			break;
 
 		case 8:
 			channel->setMode("l", false);
 			channel->setLimit(1000);
+			response += "-l";
 			break;
 
 		case 9:
 			if (modeArg == "") {
-				response =  "You must provide a limit to be set\r\nUsage: MODE #<channel name> +l <limit>\r\n";
+				std::cout << RED << "Error manipulating modes..." << WHITE << std::endl;
+				response = IRC + ERR_NEEDMOREPARAMSNBR + client->getNickname() + channelName + " " + channelName +  " l * :You must specify a parameter for the limit mode. Syntax: <limit>." + END;
 				send(fd, response.c_str(), response.size(), 0);
 				return;
 			}
@@ -162,17 +162,14 @@ void Server::mode(std::vector<std::string> string, int fd) {
 			int limit;
 			ss >> limit;
 			channel->setLimit(limit);
+			response += "+l";
 			break;
 
 		default:
-			response = "Invalid mode\r\n";
+			response = "Invalid mode";
 			break;
 	}
 
-	response = "#" + channel->getName() +
-			   ": " + client->getNickname() + "#" + channel->getName() + ": " + client->getNickname() + " has changed mode setting\r\n"
-			  ;
-	std::vector<Client*> clients = channel->getAllClients();
-	for (size_t i = 0; i < clients.size(); i++)
-		send(clients[i]->getFd(), response.c_str(), response.size(), 0);
+	response += " " + modeArg + END;
+	send(fd, response.c_str(), response.size(), 0);
 }
